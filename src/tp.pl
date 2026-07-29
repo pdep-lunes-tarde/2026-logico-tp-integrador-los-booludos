25fcba30a5c423fa06e48cb7cfe45ffe6e92e114
jabrampuga-eng <jabrampuga@frba.utn.edu.ar>
2026-07-29 15:21:24 -0300

    parte 2-a mas tests

--- src/tp.pl
+++ src/tp.pl
@@ -1,3 +1,5 @@
+% Parte 1
+
 habitante(denken, aurbest, 1290, humano).
 habitante(voll, ende, 1200, enano).
 habitante(serie, weise, 500, elfo).
@@ -24,6 +26,32 @@ estaVivo(Persona, Anio):-
     Anio >= Nacimiento,
     Anio =< Nacimiento + AniosMaximos. 
 
+% Parte 2
+
+conoce(wirbel, 1390, presencio , rescatarHermanaWirbel , [stark , fern] , klares).
+conoce(frieren, 1390, presencio , rescatarHermanaWirbel , [stark , fern] , klares).
+conoce(lawine, 1393, cancion , destruirAura , [frieren] , weise).
+conoce(voll, 1400, libro(50) , destruirAura , [denken] , aurbest).
+conoce(serie, 1335, libro(100) , destruirReyDemonio , [frieren, himmel , heiter, eisen] , ende).
+conoce(kanne, 1375, presencio , recuperarGatoPerdido , [himmel, frieren] , weise).
+
+recuerda(Persona, Hazania, Anio):-
+    conoce(Persona, AnioConoce, presencio , Hazania , _ , _ ),
+    Anio >= AnioConoce,
+    estaVivo(Persona, Anio).
+recuerda(Persona, Hazania, Anio):-
+    conoce(Persona, AnioConoce, cancion , Hazania , _ , _),
+    Anio >= AnioConoce,
+    Anio =< AnioConoce + 15,
+    estaVivo(Persona, Anio).
+recuerda(Persona, Hazania, Anio):-
+    conoce(Persona, AnioConoce, libro(Paginas), Hazania , _ , _ ),
+    Anio >= AnioConoce,
+    Anio =< AnioConoce + Paginas,
+    estaVivo(Persona, Anio).
+
+% Tests
+
 :- begin_tests(tpIntegrador, []).
 
 test(kanne_esta_viva_en_1370):-
@@ -45,3 +73,26 @@ test(serie_esta_viva_en_5000):-
     estaVivo(serie, 5000).
 
 :- end_tests(tpIntegrador).
+
+% Tests 2
+
+test(no_recuerda_destruir_aura_en_1380_, fail):-
+    recuerda(lawine, destruirAura, 1380).
+
+test(recuerda_destruir_aura_en_1400):-
+    recuerda(lawine, destruirAura, 1400).
+
+test(no_recuerda_destruir_aura_en_1410, fail):-
+    recuerda(lawine, destruirAura, 1410).
+
+test(recuerda_destruir_aura_en_1450):-
+    recuerda(voll, destruirAura, 1450).
+
+test(no_recuerda_destruir_aura_en_1460, fail):-
+    recuerda(voll, destruirAura, 1460).
+
+test(recuerda_rescatar_hermana_en_1430):-
+    recuerda(wirbel, rescatarHermanaWirbel, 1430).
+
+test(no_recuerda_rescatar_hermana_en_1440, fail):-
+    recuerda(wirbel, rescatarHermanaWirbel, 1440).
